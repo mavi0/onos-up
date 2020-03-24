ad47ff819ac56b66686631ce6b39e394b7fb8ab9
Eleanor Davies <ellie@mavieson.co.uk>
2020-03-24 16:45:03 +0000

    pre leto

---
+++ iperf/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for i in {1..90}
+do
+    clear
+    tput cup 5 5
+    date
+    tput cup 6 5
+    echo "Waiting for onos..."
+    sleep 1
+done
+
+echo "Cloning iperf scripts..."
+rm -rf improved-bassoon
+git clone git@github.com:mavi0/improved-bassoon.git
+cd improved-bassoon/goperf
+echo "Press any key to start iperf..."
+read -n 1
+python3 tempIperf.py
+
